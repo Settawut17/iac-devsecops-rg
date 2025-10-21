3aa5c7f25b5a84dec7c99d558f8bf0f7f5174e9b
pureku <s.settawut17@hotmail.com>
2025-10-21 07:47:59 +0000

    terraform first

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_name"{
+    description = "Name of Resource Group"
+    type = string
+    default = "iac-devops-rg-19"
+
+}
+
+variable "location" {
+    description = "Region of Azure"
+    type = string
+    default = "East US"
+}
